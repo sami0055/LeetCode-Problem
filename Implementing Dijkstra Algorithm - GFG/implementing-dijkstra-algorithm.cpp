8fd1927ec37119d4f1317e788382e4f4e79a9565
Sami <83569514+sami0055@users.noreply.github.com>
2023-05-09 15:28:42 +0600

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -12,46 +12,79 @@ public:
     vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
 
-        // Create a priority queue for storing the nodes as a pair {dist,node}
-        // where dist is the distance from source to the node. 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        // // Create a priority queue for storing the nodes as a pair {dist,node}
+        // // where dist is the distance from source to the node. 
+        // priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-        // Initialising distTo list with a large number to
-        // indicate the nodes are unvisited initially.
-        // This list contains distance from source to the nodes.
-        vector<int> distTo(V, INT_MAX);
+        // // Initialising distTo list with a large number to
+        // // indicate the nodes are unvisited initially.
+        // // This list contains distance from source to the nodes.
+        // vector<int> distTo(V, INT_MAX);
 
-        // Source initialised with dist=0.
-        distTo[S] = 0;
-        pq.push({0, S});
+        // // Source initialised with dist=0.
+        // distTo[S] = 0;
+        // pq.push({0, S});
 
-        // Now, pop the minimum distance node first from the min-heap
-        // and traverse for all its adjacent nodes.
-        while (!pq.empty())
-        {
-            int node = pq.top().second;
-            int dis = pq.top().first;
-            pq.pop();
+        // // Now, pop the minimum distance node first from the min-heap
+        // // and traverse for all its adjacent nodes.
+        // while (!pq.empty())
+        // {
+        //     int node = pq.top().second;
+        //     int dis = pq.top().first;
+        //     pq.pop();
 
-            // Check for all adjacent nodes of the popped out
-            // element whether the prev dist is larger than current or not.
-            for (auto it : adj[node])
-            {
-                int v = it[0];
-                int w = it[1];
-                if (dis + w < distTo[v])
-                {
-                    distTo[v] = dis + w;
+        //     // Check for all adjacent nodes of the popped out
+        //     // element whether the prev dist is larger than current or not.
+        //     for (auto it : adj[node])
+        //     {
+        //         int v = it[0];
+        //         int w = it[1];
+        //         if (dis + w < distTo[v])
+        //         {
+        //             distTo[v] = dis + w;
     
+        //             // If current distance is smaller,
+        //             // push it into the queue.
+        //             pq.push({dis + w, v});
+        //         }
+        //     }
+        // }
+        // // Return the list containing shortest distances
+        // // from source to all the nodes.
+        // return distTo;
+        
+        // =========================Implement Using Set ========================
+        
+        set<pair<int,int>>st;
+        vector<int>dist(V,INT_MAX);
+        dist[S]=0;
+        st.insert({0,S});
+       while(!st.empty()) {
+            auto it = *(st.begin()); 
+            int node = it.second; 
+            int dis = it.first; 
+            st.erase(it); 
+            
+            // Check for all adjacent nodes of the erased
+            // element whether the prev dist is larger than current or not.
+            for(auto it : adj[node]) {
+                int adjNode = it[0]; 
+                int edgW = it[1]; 
+                
+                if(dis + edgW < dist[adjNode]) {
+                    // erase if it was visited previously at 
+                    // a greater cost.
+                    if(dist[adjNode] != 1e9) 
+                        st.erase({dist[adjNode], adjNode}); 
+                        
                     // If current distance is smaller,
-                    // push it into the queue.
-                    pq.push({dis + w, v});
-                }
+                    // push it into the queue
+                    dist[adjNode] = dis + edgW; 
+                    st.insert({dist[adjNode], adjNode}); 
+                 }
             }
         }
-        // Return the list containing shortest distances
-        // from source to all the nodes.
-        return distTo;
+        return dist;
     }
 };
 
